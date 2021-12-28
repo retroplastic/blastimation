f6d1de7d09ea12ff78efcc270a1a318e61608d00
retroplastic <retroplastic@protonmail.com>
2021-12-28 17:06:05 +0100

    main: Add basic Qt gui.

--- blastimation/__main__.py
+++ blastimation/__main__.py
@@ -1,6 +1,59 @@
 #!/usr/bin/env python3
 import sys
+
+from PySide6.QtCore import QRect, Qt
+from PySide6.QtGui import QPixmap
+from PySide6.QtWidgets import QApplication, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QVBoxLayout, QWidget
+
 from rom import Rom
+
+
+class Blastimation(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.image_label = QLabel(self)
+
+        self.image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.image_label.setAlignment(Qt.AlignCenter)
+
+        screen_geometry: QRect = self.screen().geometry()
+        self.image_label.setMinimumSize(
+            screen_geometry.width() / 8, screen_geometry.height() / 8
+        )
+
+        main_layout = QVBoxLayout(self)
+        main_layout.addWidget(self.image_label)
+
+        buttons_layout = QHBoxLayout()
+
+        quit_button = QPushButton("Quit", self)
+        quit_button.setShortcut(Qt.CTRL | Qt.Key_Q)
+        quit_button.clicked.connect(self.close)
+        buttons_layout.addWidget(quit_button)
+        buttons_layout.addStretch()
+        main_layout.addLayout(buttons_layout)
+
+        self.setWindowTitle("Blastimation")
+        self.resize(300, 200)
+
+        self.pixmap = QPixmap("test/26A1C0.png")
+
+    def resizeEvent(self, event):
+        scaled_size = self.pixmap.size()
+        scaled_size.scale(self.image_label.size(), Qt.KeepAspectRatio)
+        if scaled_size != self.image_label.pixmap().size():
+            self.update_image_label()
+
+    def update_image_label(self):
+        # img = QImage(color_frame.data, w, h, ch * w, QImage.Format_RGB888)
+        self.image_label.setPixmap(
+            self.pixmap.scaled(
+                self.image_label.size(),
+                Qt.KeepAspectRatio,
+                Qt.SmoothTransformation,
+            )
+        )
 
 
 def print_usage():
@@ -18,5 +71,11 @@ def main():
     rom.print_stats()
     rom.test()
 
+    app = QApplication(sys.argv)
+    widget = Blastimation()
+    widget.show()
+    sys.exit(app.exec())
 
-main()
+
+if __name__ == "__main__":
+    main()
