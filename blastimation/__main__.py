09135bbe552c9126e13b317eb05f23ccf927386a
retroplastic <retroplastic@protonmail.com>
2021-12-28 14:53:58 +0100

    main: Unpack stuff.

--- blastimation/__main__.py
+++ blastimation/__main__.py
@@ -2,7 +2,7 @@
 import struct
 import sys
 
-from blast import Blast
+from blast import Blast, decode_blast
 
 
 def print_usage():
@@ -26,6 +26,9 @@ def main():
 
     rom_size = len(rom_bytes)
 
+    luts128 = {}
+    luts256 = {}
+
     for i in range(ROM_OFFSET, END_OFFSET, 8):
         start = struct.unpack(">I", rom_bytes[i:i+4])[0]
         size = struct.unpack(">H", rom_bytes[i+4:i+6])[0]
@@ -33,6 +36,22 @@ def main():
         assert rom_size >= start
 
         if size > 0:
-            print("%06X" % (start + ROM_OFFSET), blast_type, size)
+            address = "%06X" % (start + ROM_OFFSET)
+            to_address = "%06X" % (start + size + ROM_OFFSET)
+            print(address, to_address, blast_type, size)
+
+            encoded_bytes = rom_bytes[start + ROM_OFFSET: start + ROM_OFFSET + size]
+
+            assert len(encoded_bytes) == size
+
+            match blast_type:
+                case Blast.BLAST0:
+                    if size == 128:
+                        luts128[address] = encoded_bytes
+                    elif size == 256:
+                        luts256[address] = encoded_bytes
+                case (Blast.BLAST1_RGBA16 | Blast.BLAST2_RGBA32 | Blast.BLAST3_IA8 | Blast.BLAST6_IA8):
+                    decoded_bytes = decode_blast(blast_type, encoded_bytes)
+
 
 main()
