788542a0d2b0d766894d1a62c630f02c7fdbea76
retroplastic <retroplastic@protonmail.com>
2021-12-29 23:02:52 +0100

    rom: Implement parsing yaml file (for accurate resolutions).

--- blastimation/rom.py
+++ blastimation/rom.py
@@ -1,4 +1,5 @@
 import struct
+import yaml
 
 from blastimation.blast import Blast
 from blastimation.image import BlastImage
@@ -23,10 +24,62 @@ class Rom:
             Blast.BLAST6_IA8: {}
         }
 
-        with open(path, "rb") as f:
-            self.read(f.read())
+        if path.endswith(".yaml"):
+            self.load_yaml(path)
+        else:
+            self.load_rom(path)
 
-    def read(self, rom_bytes: bytes):
+    def load_yaml(self, yaml_path: str):
+        with open(yaml_path, "r") as f:
+            y = yaml.safe_load(f)
+
+        rom_path = y['options']['target_path']
+        with open(rom_path, "rb") as f:
+            rom_bytes = f.read()
+
+        segments = []
+        for segment in y["segments"]:
+            if len(segments) > 0:
+                if "end" not in segments[-1]:
+                    if isinstance(segment, list):
+                        segments[-1]["end"] = segment[0]
+                    elif isinstance(segment, dict):
+                        segments[-1]["end"] = segment["start"]
+
+            if isinstance(segment, dict) or len(segment) == 1:
+                continue
+
+            if segment[1] == "blast" and segment[3] != 0:
+                segment_dict = {
+                    "start": segment[0],
+                    "blast": Blast(segment[3]),
+                    "width": segment[4],
+                    "height": segment[5],
+                    "type": "blast"
+                }
+                segments.append(segment_dict)
+            elif segment[1] == "bin" and len(segment) == 3 and ".lut" in segment[2]:
+                segment_dict = {
+                    "start": segment[0],
+                    "type": "lut"
+                }
+                segments.append(segment_dict)
+
+        for s in segments:
+            address: int = s["start"]
+            data: bytes = rom_bytes[address:s["end"]]
+            if s["type"] == "lut":
+                size = (s["end"] - s["start"])
+                self.luts[size][address] = data
+            elif s["type"] == "blast":
+                blast_type: Blast = s["blast"]
+                self.images[blast_type][address] = BlastImage(blast_type, address, data,
+                                                              s["width"], s["height"])
+
+    def load_rom(self, rom_path: str):
+        with open(rom_path, "rb") as f:
+            rom_bytes = f.read()
+
         for i in range(ROM_OFFSET, END_OFFSET, 8):
             start = struct.unpack(">I", rom_bytes[i:i + 4])[0]
             size = struct.unpack(">H", rom_bytes[i + 4:i + 6])[0]
