ea1f5fae50077dd32b0d567e7bd64e5ba6b2ff7b
retroplastic <retroplastic@protonmail.com>
2021-12-30 22:49:22 +0100

    remove commented old structure. move names to yaml.

--- blastimation/rom.py
+++ blastimation/rom.py
@@ -112,66 +112,6 @@ class Rom:
                 self.images[blast_type][address] = BlastImage(blast_type, address, encoded_bytes)
 
     def init_composite_images(self):
-        """
-        self.composites.update({
-            Blast.BLAST1_RGBA16: {
-                # Vehicles
-                0x1D8420: [Comp.TopBottom, 0x1D8970],  # Ramdozer
-                0x1DA338: [Comp.TopBottom, 0x1DA898],  # Cyclone Suit
-                0x1DAE40: [Comp.TopBottom, 0x1F0498],  # Backlash
-                0x0D0288: [Comp.RightLeft, 0x0D4410],
-                0x0F25B8: [Comp.RightLeft, 0x0F27A8],
-                0x1FB810: [Comp.RightLeft, 0x1FBEC8],
-                0x112928: [Comp.RightLeft, 0x112060],
-                0x278520: [Comp.TopBottom, 0x278890],  # Muscle Car
-
-                0x15F370: [Comp.TopBottom, 0x15F4C0],
-                0x15F8C0: [Comp.TopBottom, 0x15FDC0],  # Sideswipe
-
-                0x275528: [Comp.TopBottom, 0x275AA8],  # Blast truck
-                0x273858: [Comp.TopBottom, 0x273C38],  # Van
-                0x2741A0: [Comp.TopBottom, 0x274350],  # Ballista
-                0x276FE0: [Comp.TopBottom, 0x277308],  # J-Bomb
-                0x277798: [Comp.TopBottom, 0x278000],  # Thunderfist
-                0x2746B8: [Comp.TopBottom, 0x274B20],  # Skyfall
-                0x2760C0: [Comp.TopBottom, 0x2764A8],  # American Dream
-                0x274E70: [Comp.TopBottom, 0x2751C8],  # Police car
-
-                # stones
-                0x2BC5E8: [Comp.TopBottom, 0x2BEE28],
-                # 0x2BCFE0: [Comp.TB, 0x2BE2B8],
-                0x2BCFE0: [Comp.TopBottom, 0x2BD7E0],
-
-                0x08D4C8: [Comp.TopBottom, 0x08DA10],
-
-                0x2AC268: [Comp.TopBottom, 0x2AC748],
-                0x2ACB18: [Comp.TopBottom, 0x2ACF88],
-                0x2AD370: [Comp.TopBottom, 0x2AD6F8],
-
-                # $ animation
-                0x2ADAA0: [Comp.TopBottom, 0x2ADCF8],
-                0x2ADFB0: [Comp.TopBottom, 0x2AE208],
-                0x2AE4A8: [Comp.TopBottom, 0x2AE828],
-                0x2AEC00: [Comp.TopBottom, 0x2AEF40],
-                0x2AF290: [Comp.TopBottom, 0x2AF520],
-
-                0x089EE8: [Comp.TopBottom, 0x08A840],
-
-                0x2CE088: [Comp.RightLeft, 0x2CE7B0],
-                0x13A210: [Comp.RightLeft, 0x13AAB8],
-
-                0x15A2A0: [Comp.TopBottom, 0x15A728],
-
-                0x32E570: [Comp.TopBottom, 0x32E718],  # Question mark
-
-                0x20CFF8: [Comp.Quad, 0x20DB40, 0x20E758, 0x20F120],  # Lake
-
-                0x180890: [Comp.RightLeft, 0x182330],
-
-                0x32EEF0: [Comp.TopBottom, 0x32F4F8]  # Lighthouse
-            }
-        })
-        """
         with open("composites.yaml", "r") as f:
             composites_yaml = ryaml.load(f)
 
