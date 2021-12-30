d893a558299e3d047e2073ba193fd8ce68b44259
retroplastic <retroplastic@protonmail.com>
2021-12-30 14:34:13 +0100

    experimental yalm structure

--- test/test_yaml.py
+++ test/test_yaml.py
@@ -17,6 +17,27 @@ class Test(unittest.TestCase):
                 # Vehicles
                 0x1D8420: [Comp.TB, 0x1D8970],  # Ramdozer
                 0x1DA338: [Comp.TB, 0x1DA898],  # Cyclone Suit
+                0x1FB810: [Comp.RL, 0x1FBEC8],
+            },
+            Blast.BLAST4_IA16: {
+                0x20CFF8: [Comp.Quad, 0x20DB40, 0x20E758, 0x20F120],  # Lake
+            }
+        }
+
+        composites = {
+            1: {
+                "TB": {
+                    "Ramdozer": [0x1D8420, 0x1D8970],
+                    "Cyclone Suit": [0x1DA338, 0x1DA898],
+                },
+                "RL": {
+                    "B1_RL_1FB810": [0x1FB810, 0x1FBEC8],
+                }
+            },
+            4: {
+                "Quad": {
+                    "Lake": [0x20CFF8, 0x20DB40, 0x20E758, 0x20F120],
+                }
             }
         }
 
