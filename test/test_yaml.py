3cb1a480b2653e1d085bf3b50ecc6319ae34607b
retroplastic <retroplastic@protonmail.com>
2021-12-30 14:54:33 +0100

    generate composite yaml.

--- test/test_yaml.py
+++ test/test_yaml.py
@@ -41,5 +41,49 @@ class Test(unittest.TestCase):
             }
         }
 
+        composites = {
+            1: {
+                "TB": [
+                    [0x1D8420, 0x1D8970, "Ramdozer"],
+                    [0x1DA338, 0x1DA898, "Cyclone Suit"],
+                ],
+                "RL": [
+                    [0x1FB810, 0x1FBEC8, "B1_RL_1FB810"],
+                ]
+            },
+            4: {
+                "Quad": [
+                    [0x20CFF8, 0x20DB40, 0x20E758, 0x20F120, "Lake"],
+                ]
+            }
+        }
+
         gen_yaml = ryaml.dumps(composites)
         pprint.pprint(gen_yaml)
+
+        lines = []
+
+        for blast_id, comp_dict in composites.items():
+            lines.append(f"{blast_id}:\n")
+            for comp_name, comp_list in comp_dict.items():
+                lines.append(f"  {comp_name}:\n")
+                for comp in comp_list:
+                    comp_strs = []
+                    for c in comp:
+                        if isinstance(c, int):
+                            c_str = "0x%06X" % c
+                        elif isinstance(c, str):
+                            c_str = c
+                        else:
+                            raise Exception("Unexpected element.")
+                        comp_strs.append(c_str)
+                    comp_str = ", ".join(comp_strs)
+                    lines.append(f"    - [{comp_str}]\n")
+
+        with open("test.yaml", "w") as f:
+            f.writelines(lines)
+
+        with open("test.yaml", "r") as f:
+            reloaded = ryaml.load(f)
+
+        pprint.pprint(reloaded)
