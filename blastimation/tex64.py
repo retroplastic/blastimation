6789a9888bba2dd4242af0b6bad79a9f4415c19b
retroplastic <retroplastic@protonmail.com>
2022-01-04 23:09:15 +0100

    slice rgba16 only once.

--- blastimation/tex64.py
+++ blastimation/tex64.py
@@ -5,7 +5,7 @@ from math import ceil
 
 # RRRRRGGG GGBBBBBA
 def unpack_color(data):
-    s = int.from_bytes(data[0:2], byteorder="big")
+    s = int.from_bytes(data, byteorder="big")
 
     r = (s >> 11) & 0x1F
     g = (s >>  6) & 0x1F
