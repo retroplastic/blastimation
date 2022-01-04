3ce8c60acb6abef99d451af3d9e1d0690ebbf2ab
retroplastic <retroplastic@protonmail.com>
2022-01-04 23:43:23 +0100

    test: generate some blast5 composite yaml

--- test/test_split_blast5.py
+++ test/test_split_blast5.py
@@ -14,7 +14,13 @@ class Test(unittest.TestCase):
             if image.blast == Blast.BLAST5_RGBA32:
                 blast5_addrs.append(image.address)
 
-        for a in blast5_addrs:
-            print(a)
+        #for a in blast5_addrs:
+        #    print(a)
 
-        print("len", len(blast5_addrs))
+        print("len", len(blast5_addrs), len(blast5_addrs) / 4)
+
+        chunk_size = 4
+        chunked_list = [blast5_addrs[i:i + chunk_size] for i in range(0, len(blast5_addrs), chunk_size)]
+
+        for e in chunked_list:
+            print("      - [0x%06X, 0x%06X, 0x%06X, 0x%06X]" % tuple(e))
