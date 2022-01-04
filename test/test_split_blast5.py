fa90362f26ae0dc313e141666c6a86dc02a3a928
retroplastic <retroplastic@protonmail.com>
2022-01-04 22:25:44 +0100

    wip: make blast5 splitter.

---
+++ test/test_split_blast5.py
@@ -0,0 +1,18 @@
+import unittest
+
+from blastimation.blast import Blast
+from blastimation.rom import Rom
+
+
+class Test(unittest.TestCase):
+    def test_split_blast5(self):
+        rom = Rom("blastcorps.us.v11.assets.yaml")
+
+        blast5_addrs = []
+
+        for image in rom.images.values():
+            if image.blast == Blast.BLAST5_RGBA32:
+                blast5_addrs.append(image.address)
+
+        for a in blast5_addrs:
+            print(a)
